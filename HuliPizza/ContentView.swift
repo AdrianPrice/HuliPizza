2fbc29f1053183a0ee98524378c9f89d2ebb6056
Adrian <adrianprice@zoho.com>
2020-07-30 17:28:04 +1000

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Adrian Price on 30/7/20.
+//  Copyright © 2020 Adrian Price. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
